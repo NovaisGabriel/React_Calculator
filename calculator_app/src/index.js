f3bc2dd4f77f3cb92e238aeb73757b961dfffe49
NovaisGabriel <gabrieel.novais@gmail.com>
2020-05-26 08:43:41 -0300

    Adicionando os botoes e melhorando a aparencia

--- calculator_app/src/index.js
+++ calculator_app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <div>
@@ -12,4 +11,4 @@ ReactDOM.render(
     
   , document.getElementById('root')
 );
-serviceWorker.unregister();
+
